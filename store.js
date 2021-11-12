d0e4fc85aa7d2b64fe287ba74d3043fe3b7bf4ae
Leozinhe <91293670+Leozinhe@users.noreply.github.com>
2021-11-11 22:37:49 -0300

    Create store.js

---
+++ store.js
@@ -0,0 +1,59 @@
+let STORE = {
+    questions: [
+    {//1
+    question: 'Qual destes não é uma cepa do CoronaVírus?',
+    options: ['SARS', 'MERS', 'T-VIRUS', 'COVID-19'],
+    answer: 2
+    },
+    {//2
+    question: 'O que "Corona" significa em latim?',
+    options: ['Cerveja', 'Coroa', 'Tirana', 'Chapéu'],
+    answer: 1
+    },
+    {//3
+    question: 'O coronavírus afeta tipicamente que parte do corpo humano?',
+    options: ['Sistema respiratório superior', 'Trato intestinal', 'Sinusite', 'O cérebro'],
+    answer: 0
+    },
+    {//4
+    question: 'Qual desses animais não foi encontrado como portador de uma cepa de CoronaVírus',
+    options: ['Alpacas', 'Morcegos', 'Porcos', 'Jacarés'],
+    answer: 3
+    },
+    {//5
+    question: 'Atualmente, acredita-se que o COVID-19 tenha se originado de qual desses animais?',
+    options: ['Alpacas', 'Morcegos', 'Porcos', 'Jacarés'],
+    answer: 1
+    },
+    {//6
+    question: 'A COVID-19 deve ter se originado de qual região?',
+    options: ['Tokyo, Japão', 'Cairo, Egito', 'Wuhan, China', 'Veneza, Itália'],
+    answer: 2
+    },
+    {//7
+    question: 'Qual dessas coisas você não deve fazer durante o surto atual de COVID-19?',
+    options: ['Lavar suas mãos', 'Distanciamento Social', 
+    'Viajar para outros países', 'Comprar uma quantidade alta de papéis higiênicos'],
+    answer: 2
+    },
+    {//8
+    question: 'Quem corre mais risco durante este período?',
+    options: ['Idosos', 'Pessoas com doenças autoimunes', 
+    'Pessoas com distúrbios respiratórios pré-existentes', 'Todas acima'],
+    answer: 3
+    },
+    {//9
+    question: 'Por que é importante praticar o distanciamento social neste momento?',
+    options: ['Para evitar que pessoas em risco de apanhar COVID-19',
+     'Para terminar o seu acúmulo de videogames', 
+     'Para ajudar a ler COVID-19', 
+     'Para evitar que hospitais executem testes em você'],
+    answer: 0
+    },
+    {//10
+    question: 'Existe uma cura para o COVID-19 no momento',
+    options: ['Não', 'Sim', 'Eu não sei', 'Provavelmente'],
+    answer: 0
+    }
+],
+};
